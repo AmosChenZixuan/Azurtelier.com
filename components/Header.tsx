e1f2e83036462beb6ba617235c01eeb0eb20906b
Zixuan Chen <czxvvv@foxmail.com>
2024-04-19 01:10:29 +0200

    fix:icon and logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Image src="/static/images/logo.png" width="100" height="100" alt="logo"/>
+              <Image src="/static/images/logo.png" width="100" height="100" alt="logo" />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
