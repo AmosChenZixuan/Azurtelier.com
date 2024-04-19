4932a7b1dae2f3ec9a9e9e9b0f685e84b9d5448e
Zixuan Chen <czxvvv@foxmail.com>
2024-04-19 19:32:17 +0200

    feat: top-sticky navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,9 @@ import SearchButton from './SearchButton'
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-10">
-      <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
+    <header className="fixed left-0 right-0 top-0 z-50 rounded-lg bg-white/75 py-10 backdrop-blur dark:bg-black/75">
+      <div className="mx-auto flex max-w-4xl items-center justify-between px-3 xl:max-w-5xl xl:px-0">
+        <Link href="/" aria-label={siteMetadata.headerTitle} className="flex items-center">
           <div className="flex items-center justify-between">
             <div className="mr-3">
               <Image src="/static/images/logo.png" width="100" height="100" alt="logo" />
@@ -24,22 +24,24 @@ const Header = () => {
             )}
           </div>
         </Link>
-      </div>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
-            >
-              {link.title}
-            </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
+        <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="hidden font-medium text-gray-900 transition duration-300
+              hover:text-primary-500 dark:text-gray-100
+              dark:hover:text-primary-500 sm:block"
+              >
+                {link.title}
+              </Link>
+            ))}
+          <SearchButton />
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
       </div>
     </header>
   )
