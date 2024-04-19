9c9ff3c7fad2b91fd4b2a82f3cad79c1dc0ae8e9
Zixuan Chen <czxvvv@foxmail.com>
2024-04-19 15:50:12 +0200

    chore: cleanup old files

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,9 +64,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/icon.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/icon.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/icon.jpg" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/icon.jpg" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/icon.jpg" />
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
