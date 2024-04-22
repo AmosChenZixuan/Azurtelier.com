e4d85b12813f1f9fc253f9e42d71ae4bfd1d133a
Zixuan Chen <czxvvv@foxmail.com>
2024-04-22 23:07:49 +0200

    blog: first CN blog with photos

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,10 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps) => (
+  <NextImage
+    className="rounded-md border-2 border-gray-300 shadow-xl dark:border-gray-800 dark:shadow-gray-800"
+    {...rest}
+  />
+)
 
 export default Image
