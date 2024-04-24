ad1cf51d0e6117ca2a0ca0c4ed0a1d156d8db159
Zixuan Chen <czxvvv@foxmail.com>
2024-04-24 20:46:37 +0200

    locale: about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,37 @@
+'use client'
 import { Authors, allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
-import { genPageMetadata } from 'app/seo'
+import { coreContent, CoreContent } from 'pliny/utils/contentlayer'
 import TypedBios from './TypedBio'
-
-export const metadata = genPageMetadata({ title: 'About' })
+import { LanguageContext } from 'utils/locale'
+import { useRef, useEffect, useContext, useState } from 'react'
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
+  const { currentLang } = useContext(LanguageContext)
+  const authorRef = useRef<Authors | null>(null)
+  const mainContentRef = useRef<CoreContent<Authors> | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    authorRef.current = allAuthors.find((p) => p.slug === `${currentLang}/default`) as Authors
+    mainContentRef.current = coreContent(authorRef.current)
+    setLoading(false)
+  }, [currentLang, loading])
+
+  if (loading || !authorRef.current || !mainContentRef.current) {
+    return (
+      <div className="flex items-center justify-center pt-40">
+        <div className="h-36 w-36 animate-spin rounded-full border-b-2 border-t-2 border-primary-400"></div>
+      </div>
+    )
+  }
 
   return (
     <>
-      <AuthorLayout content={mainContent}>
+      <AuthorLayout content={mainContentRef.current}>
         <TypedBios />
-        <MDXLayoutRenderer code={author.body.code} />
+        <MDXLayoutRenderer code={authorRef.current?.body.code} />
       </AuthorLayout>
     </>
   )
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,7 +1,9 @@
+'use client'
 import { ReactNode } from 'react'
 import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
+import { useTranslation } from 'utils/locale'
 
 interface Props {
   children: ReactNode
@@ -10,15 +12,16 @@ interface Props {
 
 export default function AuthorLayout({ children, content }: Props) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github } = content
+  const { t } = useTranslation()
 
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            {t('menu_about')}
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">目前可以公开的情报</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{t('desc_about')}</p>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="top-20 flex flex-col items-center space-x-2 pt-8 xl:sticky">
@@ -33,7 +36,7 @@ export default function AuthorLayout({ children, content }: Props) {
             )}
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <div className="text-gray-700 dark:text-gray-300">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
