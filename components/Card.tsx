e91ccebf7774ae7265d50a794cdd01dc2f403d7b
Amos <czxvvv@foxmail.com>
2024-04-24 14:38:57 +0200

    fix: project card title color transit delay

--- components/Card.tsx
+++ components/Card.tsx
@@ -30,7 +30,7 @@ const Card = ({ title, description, imgSrc, href }) => (
           />
         ))}
       <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight text-black dark:text-white">
           {href ? (
             <Link href={href} aria-label={`Link to ${title}`}>
               {title}
