aa2c13935a5d57af01876efda657b350356f5413
Zixuan Chen <czxvvv@foxmail.com>
2024-04-28 23:35:59 +0200

    feat: additional about page for extra authors

---
+++ app/about/[...author]/page.tsx
@@ -0,0 +1,25 @@
+import { Authors, allAuthors } from 'contentlayer/generated'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { coreContent } from 'pliny/utils/contentlayer'
+
+export const generateStaticParams = async () => {
+  const paths = allAuthors.map((p) => ({ author: p.slug.split('/') }))
+  return paths
+}
+
+export default async function Page({ params }: { params: { author: string[] } }) {
+  const locale = params.author[0]
+  const name = params.author[1]
+
+  const authorPage = allAuthors.find((p) => p.slug === `${locale}/${name}`) as Authors
+  const mainContent = coreContent(authorPage)
+
+  return (
+    <>
+      <AuthorLayout content={mainContent}>
+        <MDXLayoutRenderer code={authorPage.body.code} />
+      </AuthorLayout>
+    </>
+  )
+}
