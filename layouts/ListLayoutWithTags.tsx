6efc320c04b4e6464ce00e4fc94f30c57c26d1ea
Zixuan Chen <czxvvv@foxmail.com>
2024-05-01 17:36:52 +0200

    ui: add card container to blogs listlayout

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -87,8 +87,8 @@ export default function ListLayoutWithTags({
             {t('blog_title')}
           </h1>
         </div>
-        <div className="flex sm:space-x-24">
-          <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded-xl bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
+        <div className="flex sm:space-x-12">
+          <div className="card hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto border bg-gray-50 pt-5 dark:bg-gray-900/70 sm:flex">
             <div className="px-6 py-4">
               {pathname.startsWith('/blog') ? (
                 <h3 className="font-bold uppercase text-primary-500">{t('blog_title')}</h3>
@@ -123,12 +123,15 @@ export default function ListLayoutWithTags({
               </ul>
             </div>
           </div>
-          <div>
+          <div className="w-full">
             <ul>
               {displayPosts.map((post) => {
                 const { path, date, title, summary, tags } = post
                 return (
-                  <li key={path} className="py-5">
+                  <li
+                    key={path}
+                    className="card mb-5 transform border p-5 duration-300 hover:scale-[1.02]"
+                  >
                     <article className="flex flex-col space-y-2 xl:space-y-0">
                       <dl>
                         <dt className="sr-only">Published on</dt>
