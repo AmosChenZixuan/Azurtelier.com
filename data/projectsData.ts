f63267fa27a1976e16f58b2e65f2e0d8e06f0529
Zixuan Chen <czxvvv@foxmail.com>
2024-05-01 17:43:36 +0200

    new: project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,11 +1,18 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
+  {
+    title: 'Personal Website',
+    description: `A Next.js, Tailwind CSS project which is based on Next App directory with React Server Component and uses Contentlayer to manage markdown content. 
+    It is my go-to place for sharing recent discoveries and experiences, as well as testing out front end technologies.`,
+    imgSrc: '/static/images/banner.png',
+    href: 'https://github.com/AmosChenZixuan/amoschenzixuan.github.io',
+  },
   {
     title: 'Santorini',
     description: `My java and react implementation of a strategy-based board game for 2 players, inspired by the cliffside villages of Santorini Island in Greece. 
