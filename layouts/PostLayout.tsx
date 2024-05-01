21bb4a47047c23333790743a795f4de59ec759a7
Zixuan Chen <czxvvv@foxmail.com>
2024-05-01 18:43:01 +0200

    feat: href links to authors in the blog page

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -67,34 +67,38 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   {authorDetails
                     .filter((author) => author.slug.startsWith(currentLang))
                     .map((author) => (
-                      <li className="flex items-center space-x-2" key={author.slug}>
-                        {author.avatar && (
-                          <Image
-                            src={author.avatar}
-                            width={38}
-                            height={38}
-                            alt="avatar"
-                            className="h-10 w-10 rounded-full"
-                          />
-                        )}
-                        <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                          <dt className="sr-only">Name</dt>
-                          <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                          <dt className="sr-only">Twitter</dt>
-                          <dd>
-                            {author.twitter && (
-                              <Link
-                                href={author.twitter}
-                                className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                              >
-                                {author.twitter
-                                  .replace('https://twitter.com/', '@')
-                                  .replace('https://x.com/', '@')}
-                              </Link>
-                            )}
-                          </dd>
-                        </dl>
-                      </li>
+                      <Link key={author.name} href={`/about/${author.slug}`}>
+                        <li className="mb-5 flex items-center space-x-2" key={author.slug}>
+                          {author.avatar && (
+                            <Image
+                              src={author.avatar}
+                              width={38}
+                              height={38}
+                              alt="avatar"
+                              className="h-12 w-12 rounded-full"
+                            />
+                          )}
+                          <dl className="whitespace-nowrap text-sm font-medium leading-5">
+                            <dt className="sr-only">Name</dt>
+                            <dd className="text-lg text-gray-900 dark:text-gray-100">
+                              {author.name}
+                            </dd>
+                            <dt className="sr-only">Twitter</dt>
+                            <dd>
+                              {author.twitter && (
+                                <Link
+                                  href={author.twitter}
+                                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                                >
+                                  {author.twitter
+                                    .replace('https://twitter.com/', '@')
+                                    .replace('https://x.com/', '@')}
+                                </Link>
+                              )}
+                            </dd>
+                          </dl>
+                        </li>
+                      </Link>
                     ))}
                 </ul>
               </dd>
