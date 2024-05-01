0e80179cc93749c00d1899bde8df87ef8a48fd92
Zixuan Chen <czxvvv@foxmail.com>
2024-05-01 18:33:40 +0200

    chore: re-route home page to blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
+import BlogPage from './blog/page'
 
 export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  // const sortedPosts = sortPosts(allBlogs)
+  // const posts = allCoreContent(sortedPosts)
+  // return <Main posts={posts} />
+  return <BlogPage />
 }
