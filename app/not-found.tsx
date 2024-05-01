71b354da27ef9ff5a6224aca051d4b31dd728e6c
Zixuan Chen <czxvvv@foxmail.com>
2024-05-01 19:51:54 +0200

    ui: 404 page update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,17 +2,15 @@ import Link from '@/components/Link'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          404
-        </h1>
+    <div className="flex flex-col items-start justify-start xl:mt-24 xl:flex-row xl:items-center xl:justify-center xl:space-x-6">
+      <div className="flex flex-col items-center space-x-2 pb-8 pt-6">
+        <img src="/static/images/404.jpeg" className="h-80 w-80"></img>
       </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+      <div className="max-w-xl xl:border-l-2 xl:pl-20">
+        <p className="mb-4 text-xl font-bold leading-normal xl:text-2xl">
+          404: This page is not available yet.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">But dont worry, let me take you back to our homepage.</p>
         <Link
           href="/"
           className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
