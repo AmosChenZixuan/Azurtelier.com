505bbed17661b295a28abd4e157601b855000a75
Zixuan Chen <czxvvv@foxmail.com>
2024-05-04 03:03:12 +0200

    feat: add civitai link to GalleryPreviewer

--- components/home/GalleryPreviewBox.tsx
+++ components/home/GalleryPreviewBox.tsx
@@ -1,15 +1,20 @@
+import Link from '@/components/Link'
+import Image from '@/components/Image'
+
 export default function GalleryPreviewBox() {
   return (
     <div
       className={`card bg-pink-blue-animated sticky right-0 min-w-[4rem] max-w-[25%] flex-grow overflow-hidden`}
     >
-      <img
-        src="/static/gallary/art/ai/azuki/charming_smile.jpeg"
-        alt="azuki"
-        className="h-full w-full object-cover"
-        width={832}
-        height={1216}
-      />
+      <Link href="https://civitai.com/user/Azurtelier">
+        <Image
+          src="/static/gallary/art/ai/azuki/charming_smile.jpeg"
+          alt="azuki"
+          className="h-full w-full object-cover"
+          width={832}
+          height={1216}
+        />
+      </Link>
     </div>
   )
 }
