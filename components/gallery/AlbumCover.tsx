52ec223fcd4440a51c291669b20f05bad0557881
Zixuan Chen <czxvvv@foxmail.com>
2024-05-14 23:34:00 +0200

    feat: add screen width threshold for hiding album info

--- components/gallery/AlbumCover.tsx
+++ components/gallery/AlbumCover.tsx
@@ -10,13 +10,17 @@ interface AlbumCoverProps {
 export default function AlbumCover({ post }: AlbumCoverProps) {
   return (
     <div className="group relative m-2 overflow-hidden rounded-xl">
-      <section className="absolute bottom-0 left-0 z-10 w-full bg-black bg-opacity-50 p-2 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+      <section className="absolute bottom-0 left-0 z-10 w-full bg-black bg-opacity-50 p-2 transition-opacity duration-300 group-hover:opacity-100 xl:opacity-0">
         <Link href={post.path}>
           <h2 className="text-lg font-bold text-primary-50">{post.title}</h2>
         </Link>
         <div className="flex flex-wrap">
           {post.tags?.map((tag) => (
-            <Tag key={tag} text={tag} className="text-primary mr-1 rounded-xl border p-1 text-sm" />
+            <Tag
+              key={tag}
+              text={tag}
+              className="mr-1 rounded-xl border p-1 text-sm text-primary-300"
+            />
           ))}
         </div>
       </section>
