8d8426d30ee9a18bbdd8ccbc2b26c8433ceb24c1
Zixuan Chen <czxvvv@foxmail.com>
2024-05-23 18:00:06 +0200

    fix: photo nows get the dynamic index from overlay instead of init index

--- components/gallery/photo.tsx
+++ components/gallery/photo.tsx
@@ -24,7 +24,7 @@ const Photo = ({ useOverlay = true, index = 0, ...rest }: OverlayImageProps) => 
     if (useOverlay) {
       setIsOverlayVisible(true)
       setImageList(rest.imagelist as string[])
-      setIndex(index)
+      setIndex(overlayIndex)
       setCallerId(rest.id || '')
     }
   }
